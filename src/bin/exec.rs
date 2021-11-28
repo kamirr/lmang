2e99d27f6ca7e2e8a7885eb84f67adc3e5c2771b
Kamil Koczurek <koczurekk@gmail.com>
2021-11-28 02:17:05 +0100

    optimize via [profile.release] settings and MiMalloc

--- src/bin/exec.rs
+++ src/bin/exec.rs
@@ -1,5 +1,8 @@
 use lmang_lib::{builtins::Builtins, env::Env, expr::Expr, val::Val};
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 fn main() {
     let path = std::env::args().nth(1).expect("no file given");
     let code = std::fs::read_to_string(path).unwrap();
